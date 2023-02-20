3d55cdf3ba457ae860d1ade777c53b825040ba51
xuandatpro <121415783+xuandatpro@users.noreply.github.com>
2023-02-21 00:27:50 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+    {
+      path: '/about',
+      element: <div>About Us</div>
+    },
+    {
+      path:'/h1',
+      element:<h1>hello xuandat</h1>
+    }
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider
+    router={router}>
+  </RouterProvider>
   );
 }
 
